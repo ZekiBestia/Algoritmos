d689169209ea08d3e749819e1f918258eef18ca8
ZekiBestia <ezequielor73@gmail.com>
2023-09-17 14:54:53 -0600

    Se agrego metodo de permutaciones y Combinaciones

---
+++ Methods.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 11:13:05 2023
+
+@author: zekibestia
+"""
+# Permutaciones
+'''
+ PERMUTACIONES
+     nPr= n!/(n-r)!
+     Organizar elementos en orden
+ COMBINACIONES
+     Seleccionar objetos de un grupo y el orden no importa
+     nCr= nPr/r!
+     
+'''
+import numpy as np
+import math
+import random
+
+class MathSolver:
+  #  Methods for solving mathematical problems
+    
+    '''
+    1.RaizCuadrada
+    '''
+    @staticmethod
+    def raizMath(numero):
+        # Calcular la raíz cuadrada del número proporcionado
+        raiz_cuadrada = math.sqrt(numero)
+        return raiz_cuadrada
+    
+    @staticmethod 
+    def raizNewton_Rapson(n):
+        aproximacion = n / 2
+        precision = 0.00001  
+
+        while abs(aproximacion * aproximacion - n) > precision:
+            aproximacion = 0.5 * (aproximacion + n / aproximacion)
+        return aproximacion
+    '''
+    2.Ecuacion de Segundo Grado
+    '''
+    @staticmethod 
+# 2 Resolviendo una ecuación de segundo grado, sin usar el módulo math
+# Función para calcular las raíces de una ecuación cuadrática
+    def calcular_raices(a, b, c):
+     discriminante = b**2 - 4*a*c
+
+     if discriminante > 0:
+        raiz1 = (-b + discriminante**0.5) / (2*a)
+        raiz2 = (-b - discriminante**0.5) / (2*a)
+        return raiz1, raiz2
+     elif discriminante == 0:
+        raiz_unica = -b / (2*a)
+        return raiz_unica
+     else:
+        # Raíces complejas
+        parte_real = -b / (2*a)
+        parte_imaginaria = (abs(discriminante)**0.5) / (2*a)
+        raiz1 = complex(parte_real, parte_imaginaria)
+        raiz2 = complex(parte_real, -parte_imaginaria)
+        return raiz1, raiz2
+
+    @staticmethod 
+    def ecuNumpy(a,b,c):
+        soluciones=np.roots([a,b,c])
+        return soluciones
+    
+  
+    '''
+    3.Calcular PI
+    '''
+    @staticmethod 
+    def calcular_pi_Math():
+        pi = math.pi
+        print("El valor de pi es:", pi)
+
+    @staticmethod 
+    def calcular_pi_leibniz(n):
+        suma = 0
+        for i in range(n):
+            termino = (-1) ** i / (2 * i + 1)
+            suma += termino
+        pi_aproximado = 4 * suma
+        return pi_aproximado
+    '''
+    4.Calcular Factorial
+    '''
+    @staticmethod 
+    def calcularFactorialMath(n):
+        if n < 0:
+            return 
+        elif n == 0:
+            return 1
+        else:
+            return math.factorial(n)
+
+    @staticmethod
+    def calcularFactorial(n):
+        fact = 1
+        while n > 0:
+            fact *= n
+            n -= 1
+        return fact
+    
+    '''
+    5.Contar apariciones de un numero
+    en una lista
+    '''
+    @staticmethod
+    def contar_apariciones(lista, c):
+        contador = 0
+        for i in lista:
+            if i == c:
+                contador += 1
+        return contador
+
+    @staticmethod
+    def listaAleatorios(n,r):
+        lista = [0] * n
+        for i in range(n):
+            lista[i] = random.randint(1,r)
+        return lista
+    
+    
+    def encontrar_numeros_consecutivos(lista, umbral):
+     numeros_consecutivos = []
+
+     for i in range(len(lista) - 1):
+        if lista[i] > umbral and lista[i + 1] > umbral:
+            numeros_consecutivos.append((lista[i], lista[i + 1]))
+
+     return numeros_consecutivos
+
+    '''
+    7.Regresar el indice del predecesor menor de una lista
+    de cadenas
+    '''
+    @staticmethod
+    def indice_procesador_menor(lista):
+        if not lista:
+            return None  # Manejamos el caso de una lista vacía
+
+        menor_longitud = len(lista[0])
+        indice_menor = 0
+
+        for i in range(1, len(lista)):
+            longitud_actual = len(lista[i])
+            if longitud_actual < menor_longitud:
+                menor_longitud = longitud_actual
+                indice_menor = i
+        return indice_menor
+    
+    @staticmethod
+    def obtnerCadena():
+        while True:
+            cadena = input("Ingresa una cadena: ")
+            if cadena.strip():  # Verifica si la cadena no está vacía después de quitar espacios en blanco
+                return cadena
+            else:
+                print("La cadena no puede estar vacía. Inténtalo de nuevo.")
+                
+
+    @staticmethod
+    def nPr(n, r):
+        factorial_n = MathSolver.calcularFactorial(n)
+        factorial_temp = MathSolver.calcularFactorial(n - r)
+        permutacion = factorial_n / factorial_temp
+        return permutacion
+
+    @staticmethod
+    def nCr(n, r):
+        permu = MathSolver.nPr(n, r)
+        factorial_n = MathSolver.calcularFactorial(r)
+        combinacion = permu / factorial_n
+        return combinacion
+
+    @staticmethod
+    def generarPermuta(n, r, permuta=[]):
+        if len(permuta) == r:
+            yield permuta[:]
+        else:
+            for i in n:
+                if i not in permuta:
+                    permuta.append(i)
+                    yield from MathSolver.generarPermuta(n, r, permuta)
+                    permuta.pop()
+                    
+    @staticmethod
+    def generarCombina(n, r, combi=[], i=0):
+        if len(combi) == r:
+            yield combi[:]
+            return
+        if i == len(n):
+            return
+        combi.append(n[i])
+        yield from MathSolver.generarCombina(n, r, combi, i + 1)
+        combi.pop()
+        yield from MathSolver.generarCombina(n, r, combi, i + 1)
+        
+   
--- portada.py
+++ portada.py
@@ -6,37 +6,11 @@ Created on Sat Sep 16 15:33:19 2023
 @author: zekibestia
 """
 
-from Metodos import Metodos
-import math
-import os
-#OBJETO
-calcular= Metodos()
-calcular2= Metodos()
-
-def limpiar_pantalla():
-    sistema = os.name
-    if sistema == 'nt':  # Windows
-        os.system('cls')
-    else:  # Unix/Linux
-        os.system('clear')
-
-def pedir_numero():
-    while True:
-        try:
-            numero = int(input("Digite el número: "))
-            return numero  # Retorna el número si la entrada es válida
-        except ValueError:
-            print("Error: Debe ingresar un número válido. Inténtelo de nuevo.")
-            
-def pedirABC():
-    while True:
-        try:
-            a = int(input("Digite el número para a : "))# Retorna el número si la entrada es válida
-            b= int(input("Digite el número para b : "))
-            c = int(input("Digite el número para c: "))
-            return a,b,c
-        except ValueError:
-            print("Error: Debe ingresar un número válido. Inténtelo de nuevo.")
+from Methods import MathSolver
+from MethodsIn import UserInterface
+calcular= MathSolver()
+user=UserInterface()
+contador=0
 
 # Crea un diccionario que mapea las opciones a las descripciones
 opciones = {
@@ -48,79 +22,101 @@ opciones = {
     '6': "Dos Números Grandes Consecutivos",
     '7': "Índice del Predecesor Menor",
     '8': "Suma de Serie",
-    '9': "Salir"
+    '9': "Permutaciones",
+    '10': "Combinaciones",
+    '11': "Salir"
 }
 
 while True:
-    limpiar_pantalla()
-    print("-------MENU--------")
+    user.limpiar_pantalla()
+    user.portada()
+    print("----------MENU------------")
     for opcion, descripcion in opciones.items():
-        print(f"{opcion}. {descripcion}")
+        print(f"------{opcion}. {descripcion}")
 
-    opcion = input("Elige una opción (1-9): ")
-
+    opcion = input("Elige una opción (1-11): ")
     descripcion = opciones.get(opcion, "Opción no válida")
-
+    
     if opcion == '1':
         print("Calcular Raiz Cuadrada")
-        n=pedir_numero()
+        n=user.pedir_numero()
         raizM=calcular.raizMath(n)
         raizNewtonRapson=calcular.raizNewton_Rapson(n)
         print(f"La raiz con Math es: {raizM}")
         print(f"La raiz con el metodo Newton-Rapson es: {raizNewtonRapson}")
-        
-
     elif opcion == '2':
         # Agrega aquí tu código para resolver la ecuación de segundo grado
         print("Función para resolver ecuación de segundo grado.")
         print("ax^2 + bx + c = 0.")
-        a,b,c=pedirABC()
+        a,b,c=user.pedirABC()
         ecu = calcular.calcular_raices(a, b, c)  # Método sin math
         ecuNumpy=calcular.ecuNumpy(a, b, c)
         print(f"La solución de la ecuación sin math es: {ecu}")
         print(f"La solución de la ecuación con numpy es: {ecuNumpy}")
         
-
     elif opcion == '3':
         
         pimath=calcular.calcular_pi_Math()
         leibniz=calcular.calcular_pi_leibniz(100000)
         print(f"El valor de PI por leibniz{leibniz}")
         
-
     elif opcion == '4':
-        n = pedir_numero()
+        n =user.pedir_numero()
         facMath=calcular.calcularFactorialMath(n)
         fac=calcular.calcularFactorial(n)
         print(f"Factorial por Math {facMath}")
-        print(f"Factorial {fac}")
-        
+        print(f"Factorial {fac}")    
 
     elif opcion == '5':
         # Agrega aquí tu código para contar las veces que ocurre un elemento en una lista
         print("Función para contar las veces que ocurre un elemento en una lista.")
+        n,r,c=user.pedirListRangeNum()
+        lista=calcular.listaAleatorios(n,r)
+        conta=calcular.contar_apariciones(lista, c)
+        print(f"La lista es la siguente : {lista}")
+        print(f"El numero de apariciones es : {conta}")
 
     elif opcion == '6':
         # Agrega aquí tu código para encontrar dos números grandes consecutivos en una lista
-        print("Función para encontrar dos números grandes consecutivos en una lista.")
+        print("Función para encontrar dos números grandes consecutivos en una lista.")    
 
     elif opcion == '7':
         # Agrega aquí tu código para encontrar el índice del predecesor menor en una lista de cadenas
         print("Función para encontrar el índice del predecesor menor en una lista de cadenas.")
-        lista=["JuanCARLOS","Pedro","Juan"]
+        lista=["TOMAS","BRUNO","ELIE","DAN","ZEKE"]
         iPredecesorMenor=calcular.indice_procesador_menor(lista)
         print("El indice del predecesor menor es: " + str(iPredecesorMenor))
 
     elif opcion == '8':
         # Agrega aquí tu código para calcular la suma de una serie
         print("Función para calcular la suma de una serie.")
-
+        
     elif opcion == '9':
+        print("Permutaciones")
+        n,r=user.pedirNyR()
+        perm=calcular.generarPermuta(n, r)
+        for i in perm:
+         contador+=1
+         print(i)
+        print(f"Permutaciones Generadas: {contador}")
+        contador=0
+        
+    elif opcion =='10':
+        print("Combinaciones")
+        n,r=user.pedirNyR()
+        combi=calcular.generarCombina(n,r)
+        for i in combi:
+         print(i)
+         contador+=1
+        print(f"Combinaciones Generadas: {contador}")
+        contador=0
+        
+    elif opcion == '11':
         print("Saliendo del programa. ¡Hasta luego!")
         break
-
+        
     else:
-        print("Opción no válida. Introduce un número del 1 al 9.")
+        print("Opción no válida. Introduce un número del 1 al 11.")
 
     # Preguntar al usuario si desea volver al menú o salir
     respuesta = input("¿Desea volver al menú (S/N)? ").strip().lower()
